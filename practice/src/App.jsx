391b81fba0e62c99683543ce17aa67df56729ab7
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2025-09-17 00:06:01 +0530

    new

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+const App = () => {
 
-function App() {
-  const [count, setCount] = useState(0)
+useStateSnippet
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+  let user =  "harsh"; //object assignment
+  function abc() {
+    console.log(user);
+    user = " thakur harsh singh";
+    console.log(user);
+  }
+  const a = 5;  //value assignment
+
+  return ( <div>
+    <h1>menu {a}</h1>
+    <h2>products {user} </h2> 
+    <button onClick={abc}>change user</button>
+  </div> );
 }
-
-export default App
+ 
+export default App;
