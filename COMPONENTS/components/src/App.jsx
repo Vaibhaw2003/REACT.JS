8c62893cdefd164e30b0713c4c7fd6da0dbaa614
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2025-09-20 02:15:39 +0530

    compo

---
+++ COMPONENTS/components/src/App.jsx
@@ -0,0 +1,15 @@
+const App = () => {
+  return ( 
+    <>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <h1>App Component</h1>
+      <div>
+        <h4>jjj</h4>
+        <h4>about</h4>
+      </div>
+    </nav>
+    </>
+   );
+}
+ 
+export default App;
