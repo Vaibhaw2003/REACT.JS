cddb6628703d9630931cfbf5c8141108aa3ac1d5
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2025-09-21 02:17:55 +0530

    compo

--- COMPONENTS/components/src/App.jsx
+++ COMPONENTS/components/src/App.jsx
@@ -1,13 +1,25 @@
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+
 const App = () => {
   return ( 
     <>
+   <Header />
+   <Footer />
+   
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <h1>App Component</h1>
-      <div>
-        <h4>jjj</h4>
+      <div className="flex p-2 gap-8 items-center">
+        <h4>product</h4>
+        <h4>items</h4>
+        <h4>menu</h4>
         <h4>about</h4>
+        
       </div>
     </nav>
+    <Navbar />
     </>
    );
 }
---
+++ COMPONENTS/components/src/components/Header.jsx
@@ -0,0 +1,7 @@
+const Header = () => {
+    return ( <div>
+        mai hu header
+    </div> );
+}
+ 
+export default Header;
