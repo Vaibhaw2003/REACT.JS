0e6b6b86b1784601f58f0de717d26353ecb9affc
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2025-09-21 03:28:33 +0530

    API

--- COMPONENTS/components/src/App.jsx
+++ COMPONENTS/components/src/App.jsx
@@ -1,17 +1,33 @@
-import React from "react";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Navbar from "./components/Navbar";
+import React, { use, useState } from "react";
+import axios from "axios";
 
 const App = () => {
+
+  const [data, setData] = useState([]);
+
+  const getData = async()=>{
+    const response =await axios.get('https://picsum.photos/id/{image_id}/{width}/{height}')
+    setData(response.data)
+    console.log(data)
+  }
+
+
+
   return ( 
     <>
-   <Header />
-   <Footer />
-   
+
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <h1>App Component</h1>
       <div className="flex p-2 gap-8 items-center">
+
+        <button onClick={getData}>submit</button>
+        {
+          data.map(function(elem, idx){
+            return <div key={idx}>
+              <img src="elem.downloade_url" alt="" />
+            </div>
+          })
+        }
         <h4>product</h4>
         <h4>items</h4>
         <h4>menu</h4>
@@ -19,7 +35,6 @@ const App = () => {
         
       </div>
     </nav>
-    <Navbar />
     </>
    );
 }
---
+++ COMPONENTS/components/src/components/Card.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+const Card = () => {
+    return ( <div>
+        <h1>product this</h1>
+        <h1>details</h1>
+        <button>add items</button>
+    </div> );
+}
+ 
+export default Card;
