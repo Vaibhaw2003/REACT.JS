bbb5aa7b174f28ff20f58c09c3d97755b913ee9a
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2025-10-01 01:38:36 +0530

    comlete

--- PROJECT_01/mini_project/src/Components/Card.jsx
+++ PROJECT_01/mini_project/src/Components/Card.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
-import { LuFileSpreadsheet } from "react-icons/lu";
+import { LuFileSpreadsheet } from "react-icons/lu"
+import { motion, scale } from "motion/react"
 
-const Card = () => {
+const Card = ({data, reference }) => {
   return (
     
-         <div className='w-60 h-72 rounded-[20px] bg-sky-800 text-white p-5 m-5'>
+         <motion.div drag dragConstraints={reference} whileDrag={{scale: 1.2}} className='w-60 h-72 rounded-[20px] bg-zinc-600 text-white p-5 m-5 overflow-hidden'>
              <LuFileSpreadsheet />
-             <p className='text-sm font-semibold mt-4  '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, dolore neque nam facere aliquam.</p>
-             <div className='footer absolute bottom-0 bg-green-300 w-full h-20 left-0 overflow-hiden '></div>
+             <img className=' flex w-20 h-20 rounded items-center' src="https://i.pinimg.com/originals/3d/a7/a1/3da7a11be8bc9b0d08e2098fdd3a5f29.jpg" alt="" />
+             <p className='text-sm font-semibold mt-4  '>{data.desc}</p>
+             <div className=' absolute w-full h-10 bottom-0 bg-red-500 left-0 '></div>
+              <button className='w-full h-10 bg-zinc-800 rounded-lg mt-5 font-semibold hover:bg-blue-700'>Get Started</button>
 
-         </div>
+        </motion.div>
   )
 }
 
--- PROJECT_01/mini_project/src/Components/Foregraund.jsx
+++ PROJECT_01/mini_project/src/Components/Foregraund.jsx
@@ -1,14 +1,45 @@
 import React from 'react'
 import Card from './Card'
+import { u } from 'motion/react-client';
+
 
 
 const Foregraund = () => {
+  
+  const ref = React.useRef(null);
+
+  const data = 
+  [{
+    desc :"I am vaibhaw singh, from varanasi,india.",
+     filesize: ".9mb"
+  },
+
+{
+    desc :"I am vaibhaw singh, from varanasi,india.",
+     filesize: ".9mb"
+  },
+  {
+    desc :"I am vaibhaw singh, from varanasi,india i will creat a react app.",
+     filesize: ".9mb"
+  },
+  {
+    desc :"I am vaibhaw singh, from varanasi,india.",
+     filesize: ".9mb"
+  },
+
+{
+    desc :"I am vaibhaw singh, from varanasi,india.",
+     filesize: ".9mb"
+  }];
+  
+
+
   return (
     <div>
-        <div className='z-[3] top-0 left-0  w-full h-full fixed '>
-        <Card /> 
-
-
+        <div ref={ref} className='z-[3] top-0 left-0  w-full h-full fixed p-5 row-span-2 row-start-2 flex gap-5 justify-center flex-wrap overflow-auto '>
+        {data.map((items ,index)=>(
+          <Card data ={items} reference ={ref}/>
+        ))}
 
         </div>
     </div>
