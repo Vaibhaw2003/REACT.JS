aba2865dc70f70471c065f7c581f1b8e5712dcaa
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2025-10-21 12:54:15 +0530

    hulk web

---
+++ practice 07/practice-07/src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <>
+          <nav id='nav'>
+            <a href="home">HOME</a>
+            <a href="explore">EXPLORE</a>
+            <a href="ABOUT">ABOUT</a>
+            <a href="CONTACT US">CONTACT US</a>
+          </nav>
+
+          <div id='body'>
+            <img src="https://tse3.mm.bing.net/th/id/OIP.LEXM9Ey9MqapCpGxmchYSgHaHa?pid=Api&P=0&h=180" alt="" />
+            <img src="https://tse3.mm.bing.net/th/id/OIP.LJtUSOSKI6bKCzUvKFNMeQHaEK?pid=Api&P=0&h=180" alt="" />
+          </div>
+
+          <div id='footer'>
+            <footer>
+              <h1></h1>
+              <h1></h1>
+              <h1></h1>
+              <h1></h1>
+            </footer>
+          </div>
+      </>
+    </div>
+  )
+}
+
+export default App
