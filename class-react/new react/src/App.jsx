25413c0a883fc969fd06a5f33e1851ed4779f8ca
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2025-11-25 11:32:59 +0530

    new class

---
+++ class-react/new react/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Nav from './assets/COMPONENTS/Nav'
+
+const App = () => {
+  return (
+    <div>
+      <Nav/>
+    </div>
+  )
+}
+
+export default App
---
+++ class-react/new react/src/assets/COMPONENTS/Nav.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div>
+        <img src="" alt="" />
+        <a href="home">home</a>
+        <a href="product">product</a>
+        <a href="about">about</a>
+    </div>
+  )
+}
+
+export default Nav
