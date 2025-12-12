ad3963c9439a9911c135fa42683ccf9807ad0805
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2025-12-12 10:27:24 +0530

    web class 12/12/2025

--- CLASS-REACT02/class02/src/App.jsx
+++ CLASS-REACT02/class02/src/App.jsx
@@ -1,3 +1,4 @@
+/*
 const student = [
   { id: 1, name: "vaibhaw singh", age: 22 },
   { id: 2, name: "vinit shaini", age: 22 }
@@ -19,3 +20,26 @@ function App() {
 }
 
 export default App;
+*/
+
+
+const LoginStatus = ({ isLoggedIn }) => {
+  let message;
+
+  if (isLoggedIn) {
+    message = <h1>Welcome back</h1>;
+  } else {
+    message = <h1>Please login...</h1>;
+  }
+
+  return (
+    <div>
+      {message}
+    </div>
+  );
+};
+
+export default LoginStatus;
+
+
+
